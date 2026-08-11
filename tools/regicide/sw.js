d0e3a51838841e3200dcdfd0ec1513df0b9aa26e
cmondary <clement.mondary@gmail.com>
2026-08-11 10:17:48 +0200

    ADD: pwa regicide

---
+++ tools/regicide/sw.js
@@ -0,0 +1,17 @@
+const CACHE='regicide-v2'
+self.addEventListener('install',()=>self.skipWaiting())
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))
+})
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return
+  e.respondWith(
+    caches.match(e.request).then(hit=>{
+      const fetchP=fetch(e.request).then(res=>{
+        if(res.ok&&new URL(e.request.url).origin===location.origin){const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl))}
+        return res
+      }).catch(()=>hit)
+      return hit||fetchP
+    })
+  )
+})
